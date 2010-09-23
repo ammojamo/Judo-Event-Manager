09978197740ef114841db17f915d2af7784760b7
James Watmuff <james.watmuff@gmail.com>
2010-09-23 14:20:35 +0930

    Continue loading if JmDNS registration doesn't succeed within 10 seconds

--- src/au/com/jwatmuff/genericp2p/jmdns/JmDNSRegistrationService.java
+++ src/au/com/jwatmuff/genericp2p/jmdns/JmDNSRegistrationService.java
@@ -9,6 +9,7 @@ import au.com.jwatmuff.genericp2p.PeerRegistrationService;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 import org.apache.log4j.Logger;
@@ -46,26 +47,49 @@ public class JmDNSRegistrationService implements PeerRegistrationService {
         service = ServiceInfo.create(REG_TYPE, ourName, port, "Event Manager");
     }
 
+    @Override
     public void register() {
+        log.debug("Attempting to register as service (" + ourName + ")");
+
+        final AtomicBoolean failed = new AtomicBoolean(false);
+
+        // perform registration in a seperate thread so that if it blocks for a
+        // long time, we can give up waiting for it and continue
+        Thread registerThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    jmdns.registerService(service);
+                    //todo: is there a way to verify that we succesfully registered?
+                    registered = true;
+                } catch (IOException e) {
+                    failed.set(true);
+                    log.error("Unable to register with network", e);
+                    //throw new RuntimeException("Bonjour registration failed", e);
+                }
+            }
+        };
+
+        registerThread.start();
         try {
-            jmdns.registerService(service);
-            log.debug("Attempting to register as service (" + ourName + ")");
-            //todo: is there a way to verify that we succesfully registered?
-            registered = true;
-        } catch(IOException e) {
-            log.error("Unable to register with network", e);
-            throw new RuntimeException("Bonjour registration failed", e);
+            registerThread.join(10000);
+            if(!failed.get()) {
+                log.warn("Failed to register within 10 seconds, continuing..");
+            }
+        } catch(InterruptedException e) {
+            log.error("Interrupted while waiting for registration thread.");
         }
     }
 
+    @Override
     public void unregister() {
         jmdns.unregisterService(service);
         registered = false;
     }
 
     public String getOurName() {
-        if(!registered)
-            throw new RuntimeException("Invalid state - service is not running");
+        //if(!registered)
+        //    throw new RuntimeException("Invalid state - service is not running");
         return ourName;
     }
 }
