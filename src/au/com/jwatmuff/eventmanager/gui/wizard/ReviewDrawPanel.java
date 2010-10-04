9429a6d48704bf8ca16950eea96bc591a02b005e
Leonard Hall <Leonard@freespacesolutions.com.au>
2010-10-04 23:29:56 +1030

    Add Print Button to draw wizard.
    Signed-off-by: Leonard Hall <Leonard@freespacesolutions.com.au>

--- src/au/com/jwatmuff/eventmanager/gui/wizard/ReviewDrawPanel.java
+++ src/au/com/jwatmuff/eventmanager/gui/wizard/ReviewDrawPanel.java
@@ -19,9 +19,11 @@ import au.com.jwatmuff.eventmanager.model.misc.PoolLocker;
 import au.com.jwatmuff.eventmanager.model.vo.Pool;
 import au.com.jwatmuff.eventmanager.permissions.Action;
 import au.com.jwatmuff.eventmanager.permissions.PermissionChecker;
+import au.com.jwatmuff.eventmanager.print.MultipleDrawHTMLGenerator;
 import au.com.jwatmuff.eventmanager.util.GUIUtils;
 import au.com.jwatmuff.genericdb.transaction.TransactionNotifier;
 import au.com.jwatmuff.genericdb.transaction.TransactionalDatabase;
+import java.util.ArrayList;
 
 /**
  *
@@ -58,6 +60,8 @@ public class ReviewDrawPanel extends javax.swing.JPanel implements DrawWizardWin
         jScrollPane2 = new javax.swing.JScrollPane();
         fightTable = new javax.swing.JTable();
         divisionNameLabel = new javax.swing.JLabel();
+        jToolBar1 = new javax.swing.JToolBar();
+        printButton = new javax.swing.JButton();
 
         playerTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -97,8 +101,18 @@ public class ReviewDrawPanel extends javax.swing.JPanel implements DrawWizardWin
         fightTable.setRowHeight(19);
         jScrollPane2.setViewportView(fightTable);
 
-        divisionNameLabel.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        divisionNameLabel.setFont(new java.awt.Font("Tahoma", 1, 24));
         divisionNameLabel.setText("Division Name");
+
+        jToolBar1.setFloatable(false);
+
+        printButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/famfamfam/icons/silk/printer.png"))); // NOI18N
+        printButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                printButtonActionPerformed(evt);
+            }
+        });
+        jToolBar1.add(printButton);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -107,12 +121,17 @@ public class ReviewDrawPanel extends javax.swing.JPanel implements DrawWizardWin
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(divisionNameLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 489, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(10, 10, 10)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE))
-                    .addComponent(divisionNameLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 543, Short.MAX_VALUE))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(530, Short.MAX_VALUE)
+                    .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap()))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -124,8 +143,19 @@ public class ReviewDrawPanel extends javax.swing.JPanel implements DrawWizardWin
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 293, Short.MAX_VALUE)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 293, Short.MAX_VALUE))
                 .addContainerGap())
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(317, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void printButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_printButtonActionPerformed
+        ArrayList<Pool> pools = new ArrayList<Pool>();
+        pools.add(context.pool);
+        new MultipleDrawHTMLGenerator(database, pools, false).openInBrowser();
+}//GEN-LAST:event_printButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -133,7 +163,9 @@ public class ReviewDrawPanel extends javax.swing.JPanel implements DrawWizardWin
     private javax.swing.JTable fightTable;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JToolBar jToolBar1;
     private javax.swing.JTable playerTable;
+    private javax.swing.JButton printButton;
     // End of variables declaration//GEN-END:variables
 
     @Override
