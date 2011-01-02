b0c14aaddbe915311e99019ab7f0be72fab8989f
Leonard Hall <Leonard@freespacesolutions.com.au>
2011-01-02 23:41:18 +1030

    Removed my stuff from Result.java
    Signed-off-by: Leonard Hall <Leonard@freespacesolutions.com.au>

--- src/au/com/jwatmuff/eventmanager/model/vo/Result.java
+++ src/au/com/jwatmuff/eventmanager/model/vo/Result.java
@@ -19,13 +19,6 @@ import au.com.jwatmuff.genericdb.distributed.DistributableObject;
 public class Result extends DistributableObject<Integer> {
     private int fightID;
     private int[] playerIDs = new int[] {0, 0};
-//    I don't know if the specification for the finalScore is the best for this situation, maybe change it to a more appropreate structure.
-/* Leonard
-    private int[] finalScores = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private int fightTime;
-    private int[] playerScores = new int[] {0, 0};
- *
- */
 
     private int[] simpleScores = new int[] {0, 0};
     // full score, e.g. "I:0,W:1,Y:4,S:0,D:0"
@@ -58,26 +51,7 @@ public class Result extends DistributableObject<Integer> {
     public int[] getPlayerIDs() {
         return playerIDs;
     }
-/* Leonard
-    public void setFinalScores(int[] finalScores) {
-        assert finalScores != null;
-        assert finalScores.length == 10;
 
-        this.finalScores = finalScores;
-    }
-
-    public int[] getFinalScores() {
-        return finalScores;
-    }
-
-    public void setFightTime(int fightTime) {
-        this.fightTime = fightTime;
-    }
-
-    public int getFightTime() {
-        return fightTime;
-    }
-*/
     public void setEventLog(String eventLog) {
         this.eventLog = eventLog;
     }
@@ -86,7 +60,6 @@ public class Result extends DistributableObject<Integer> {
         return eventLog;
     }
 
-        //        This needs to be removed.
     public int[] getSimpleScores() {
         return simpleScores;
     }
@@ -109,12 +82,6 @@ public class Result extends DistributableObject<Integer> {
         this.scores = scores;
     }
 
-    /* Leonard
-    public int[] getPlayerScores() {
-//        This needs to be calculated using a points calculator. The points calculator should be a utility of some sort.
-        return playerScores;
-    }
-*/
     public int getDuration() {
         return duration;
     }
