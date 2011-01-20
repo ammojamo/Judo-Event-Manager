553ba031aed63876d85bb90b791bd80d09f1f330
James Watmuff <james.watmuff@gmail.com>
2011-01-20 22:54:40 +1030

    Make scoreboard score calculation code more readable and fix a potential bug

--- src/au/com/jwatmuff/eventmanager/gui/scoreboard/ScoreboardModelImpl.java
+++ src/au/com/jwatmuff/eventmanager/gui/scoreboard/ScoreboardModelImpl.java
@@ -558,7 +558,14 @@ public class ScoreboardModelImpl implements ScoreboardModel, Serializable {
             return 0;
 
         int s = score[player][type.ordinal()];
-        if(shido[1-player] == (4-type.ordinal())) s++;
+
+        /* adjust for other players shido */
+        int otherPlayer = 1 - player;
+        if((shido[otherPlayer] == 2 && type == Score.YUKO) ||
+           (shido[otherPlayer] == 3 && type == Score.WAZARI) ||
+           (shido[otherPlayer] == 4 && type == Score.IPPON)) {
+            s++;
+        }
         return s;
     }
     
