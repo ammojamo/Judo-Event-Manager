854a454468fa1e9cfd05aa43dc0027948e3209b7
James Watmuff <james.watmuff@gmail.com>
2011-02-09 22:22:56 +1030

    263: Fix null pointer exception when creating a new competition

--- src/au/com/jwatmuff/genericdb/p2p/UpdateManager.java
+++ src/au/com/jwatmuff/genericdb/p2p/UpdateManager.java
@@ -77,7 +77,9 @@ public class UpdateManager implements TransactionListener, DatabaseUpdateService
         ourID = peerManager.getUUID();
 
         Update updateFromFile = loadUpdatesFromFile();
-        updateFromFile.updateClock();
+        if(updateFromFile != null) {
+            updateFromFile.updateClock();
+        }
 
         if(updateFromFile == null) {
             log.info("Creating new update store");
