f25ebd02b2727806d404af52a3de159f0da8b730
lthall <Leoanrd@freespacesolutions.com.au>
2011-09-11 23:27:10 +0930

    Licence update and Version 9 I missed
    Signed-off-by: lthall <Leoanrd@freespacesolutions.com.au>

--- src/au/com/jwatmuff/eventmanager/Main.java
+++ src/au/com/jwatmuff/eventmanager/Main.java
@@ -51,7 +51,7 @@ public class Main {
     //internal version - bump this up whenever making a database change
     public static final String VERSION = "6";
     public static final String WINDOW_TITLE = "Event Manager 2011";
-    public static final String VISIBLE_VERSION = "Event Manager 2011 Update 8";
+    public static final String VISIBLE_VERSION = "Event Manager 2011 Update 9";
 
     private static File workingDir = new File(".");
 
